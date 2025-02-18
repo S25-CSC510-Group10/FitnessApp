5b90f8dc926e12150fbff8bc66fd32690a86bca0
Ryan McPhee <102255775+rcmcphee@users.noreply.github.com>
2025-02-17 22:57:48 -0500

    Add empty option for form's first load up

--- forms.py
+++ forms.py
@@ -41,7 +41,7 @@ class RegistrationForm(FlaskForm):
         'Height (in centimeters)', validators=[
             DataRequired(), Length(
                 min=2, max=20)])
-    goal = SelectField('Select Goal', choices=['Weight Loss', 'Muscle Gain'])
+    goal = SelectField('Select Goal', choices=['---', 'Weight Loss', 'Muscle Gain'])
     target_weight = StringField(
         'Target Weight (in kilograms)', validators=[
             DataRequired(), Length(
